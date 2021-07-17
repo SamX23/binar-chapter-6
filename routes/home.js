a24d6cd665026d1aff5508b7620f2587bfcd8804
SamX23 <skalexsong@gmail.com>
2021-07-17 23:37:56 +0700

    Update : add register validation

--- routes/home.js
+++ routes/home.js
@@ -1,34 +1,13 @@
 const express = require("express");
 const app = express.Router();
-const { User } = require("../models");
 
 // Home Page
 app.get("/", (req, res, next) => {
+  let data = req.query.user;
   res.render("index", {
     title: "Traditional Games",
-    name: "",
+    name: data,
   });
-});
-
-app.post("/", (req, res, next) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let errors = false;
-
-  User.create({
-    username: username,
-    password: password,
-  })
-    .then((user) => {
-      res.status(201).render("index", {
-        title: "Traditional Games",
-        name: user.username,
-      });
-    })
-    .catch((err) => {
-      errors = true;
-      res.status(422).flash("Cannot create users:", err);
-    });
 });
 
 module.exports = app;
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const app = express.Router();
+const { User } = require("../models");
 
 // Register Page
-app.get("/register", (req, res, next) =>
-  res.render("register", { title: "Register Page" })
-);
+app.get("/register", (req, res) => {
+  const exist = req.query.msg;
+  res.render("register", { title: "Register Page", username: exist });
+});
+
+app.post("/register", (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  })
+    .then((user) => {
+      if (!user) {
+        User.create({
+          username: req.body.username,
+          password: req.body.password,
+        })
+          .then((user) => {
+            res.status(201).redirect("/?user=" + user.username);
+          })
+          .catch((err) => {
+            res.status(422).send("Cannot create users:", err);
+          });
+      } else {
+        res.redirect("/register?msg=userexist");
+      }
+    })
+    .catch((err) => {
+      res.send("ERROR: " + err);
+    });
+});
 
 module.exports = app;
